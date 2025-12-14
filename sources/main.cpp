1b7c57ff3a668267f1b04e9e0037640e9fc0a306
Roomstream <nikitosa3051@gmail.com>
2025-12-14 20:43:07 +0300

    Создание нескольких вершин, сохраняя координаты их центров

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,7 @@
 #include "raylib.h"
-
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+#include <vector>
 #define SCREEN_WIDTH (1920)
 #define SCREEN_HEIGHT (1080)
 
@@ -11,27 +13,39 @@ int main(void)
     SetTargetFPS(120);
 
     //Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    bool showMessageBox = false;
+    std::vector<Vector2> ballCenters;
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        //ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
         //const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
         //const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
 
         Vector2 mousePosition = GetMousePosition();
-        
         //DrawTexture(texture, texture_x, texture_y, WHITE);
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) == true)
         {
-            Vector2 centerballPosition;
+            Vector2 centerballPosition = GetMousePosition();
+            ballCenters.push_back(centerballPosition);
             centerballPosition.x = GetMouseX();
             centerballPosition.y = GetMouseY();
             
             DrawCircleV(centerballPosition, 50, BEIGE);
         }
-        if (IsKeyReleased(KEY_C) == true) 
+        for (Vector2 ballCenter : ballCenters) 
         {
-            ClearBackground(RAYWHITE);
+            DrawCircleV(ballCenter, 50, BEIGE);
+        }
+        
+        if (GuiButton(Rectangle{ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+
+        if (showMessageBox)
+        {
+            int result = GuiMessageBox(Rectangle{ 85, 70, 250, 100 },
+                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+            if (result >= 0) showMessageBox = false;
         }
 
         //const char* text = "OMG! IT WORKS!";
